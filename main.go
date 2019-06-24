127d12bc0b6578f81fe9e4469c021cb8ff8535b2
ManojChandran <manoj.meparmbu@gmail.com>
2019-06-23 22:12:55 -0400

    changes made to add index, go midules

--- main.go
+++ main.go
@@ -5,26 +5,95 @@ import (
   "net/http"
   "github.com/gorilla/mux"
   "html/template"
+  "log"
+  "encoding/json"
+  "fmt"
 )
 
 var templates *template.Template
+
+type configuration struct {
+  PORT            string
+  ROOT            string
+  SHUTDOWN        string
+  STATIC          string
+  API_MSGS        string
+}
+
+var conf configuration
+
+type Page struct{
+  Title string
+}
+
+// message struct model
+type PostMsg struct {
+  UserID string `json:"UserID"`
+  Msg string `json:"Msg"`
+  Timestamp string `json:"Timestamp"`
+}
+// initialize message struct
+var postMsgs []PostMsg
+
 func init()  {
-  templates = template.Must(template.ParseGlob("template/index.html"))
+  // intialize the template
+  templates = template.Must(template.ParseFiles("template/index.html", "template/message.html"))
+  //mock data
+  postMsgs = append(postMsgs, PostMsg{
+    UserID : "mchan",
+    Msg : "hello, how r u?",
+    Timestamp : "10:00:00 jun",
+  })
+
+  // reading api paths
+  file,_ := os.Open("conf.json")
+  defer file.Close()
+
+  decoder := json.NewDecoder(file)
+  //  conf := configuration{}
+  err := decoder.Decode(&conf)
+  if err != nil {
+    fmt.Println("path file not found", err)
+  }
 }
 
 func main()  {
+
+  // Router
   r := mux.NewRouter()
   fs := http.FileServer(http.Dir("./static/"))
 
-  r.HandleFunc("/shutdown", shutdown)
-  r.HandleFunc("/", indexHandler)
-  r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
-  http.Handle("/", r)
-  http.ListenAndServe(":5000", nil)
+  // api's
+  r.HandleFunc(conf.API_MSGS, getMessages).Methods("GET")
+  r.HandleFunc(conf.API_MSGS, postMessages).Methods("POST")
+
+  r.HandleFunc(conf.SHUTDOWN, shutdown)
+  r.HandleFunc(conf.ROOT, indexHandler)
+  r.PathPrefix(conf.STATIC).Handler(http.StripPrefix(conf.STATIC, fs))
+  http.Handle(conf.ROOT, r)
+  log.Fatal(http.ListenAndServe(conf.PORT, nil))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-  templates.ExecuteTemplate(w, "index.html", nil)
+  data := &Page{
+    Title : "TITLE",
+  }
+  templates.ExecuteTemplate(w, "index.html", data)
+}
+
+// Get all getMessages
+func getMessages(w http.ResponseWriter, r *http.Request)  {
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(postMsgs)
+}
+
+// Post Messages
+func postMessages(w http.ResponseWriter, r *http.Request)  {
+  w.Header().Set("Content-Type", "application/json")
+  var postedMsg PostMsg
+  _=json.NewDecoder(r.Body).Decode(&postedMsg)
+  postMsgs = append(postMsgs, postedMsg)
+  json.NewEncoder(w).Encode(postedMsg)
 }
 
 func shutdown(w http.ResponseWriter, r *http.Request) {
